1a7d748987702aa8ee7ecd100f4b8e93769e7690
aeither <36173828+aeither@users.noreply.github.com>
2021-10-23 12:00:12 +0200

    Initial commit

---
+++ src/theme/components/index.ts
@@ -0,0 +1,75 @@
+import Accordion from './accordion';
+import Alert from './alert';
+import Badge from './badge';
+import Breadcrumb from './breadcrumb';
+import Button from './button';
+import Checkbox from './checkbox';
+import CloseButton from './close-button';
+import Code from './code';
+import Container from './container';
+import Divider from './divider';
+import Drawer from './drawer';
+import Editable from './editable';
+import Form from './form';
+import FormLabel from './form-label';
+import Heading from './heading';
+import Input from './input';
+import Kbd from './kbd';
+import Link from './link';
+import List from './list';
+import Menu from './menu';
+import Modal from './modal';
+import PinInput from './pin-input';
+import Popover from './popover';
+import Progress from './progress';
+import Select from './select';
+import Skeleton from './skeleton';
+import SkipLink from './skip-link';
+import Slider from './slider';
+import Spinner from './spinner';
+import Stat from './stat';
+import Switch from './switch';
+import Table from './table';
+import Tag from './tag';
+import Textarea from './textarea';
+import Tooltip from './tooltip';
+import FormError from './form-error';
+
+export default {
+  Accordion,
+  Alert,
+  Badge,
+  Breadcrumb,
+  Button,
+  Checkbox,
+  CloseButton,
+  Code,
+  Container,
+  Divider,
+  Drawer,
+  Editable,
+  Form,
+  FormLabel,
+  Heading,
+  Input,
+  Kbd,
+  Link,
+  List,
+  Menu,
+  Modal,
+  PinInput,
+  Popover,
+  Progress,
+  Select,
+  Skeleton,
+  SkipLink,
+  Slider,
+  Spinner,
+  Stat,
+  Switch,
+  Table,
+  Tag,
+  Textarea,
+  Tooltip,
+  FormError,
+};
