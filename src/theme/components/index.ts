5d81d803335a7f01c205b132179f31f7b728bd79
aeither <36173828+aeither@users.noreply.github.com>
2022-04-23 23:02:18 +0200

    rm chakra ui steps

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,4 +1,3 @@
-import { StepsStyleConfig as Steps } from 'chakra-ui-steps';
 import Accordion from './accordion';
 import Alert from './alert';
 import Badge from './badge';
@@ -41,7 +40,6 @@ import Textarea from './textarea';
 import Tooltip from './tooltip';
 
 export default {
-  Steps,
   Accordion,
   Alert,
   Badge,
